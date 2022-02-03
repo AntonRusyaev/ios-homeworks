33efb189cb0fe115fe831415be1d5cf3c524e462
Anton <93534558+AntonRusyaev@users.noreply.github.com>
2022-02-03 23:07:55 +0500

    added netology_IB-Instruments

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Anton Rusyaev on 02.02.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var Name: UILabel!
+    
+    @IBOutlet weak var birthDate: UILabel!
+    
+    @IBOutlet weak var City: UILabel!
+    
+    @IBOutlet weak var Signature: UITextView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
